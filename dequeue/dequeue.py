f1ee5c003847ba6166607c6c0750f491d79d9e6f
rebrunner <35457774+rebbrunner@users.noreply.github.com>
2018-09-06 10:56:17 -0400

    Add merge function
    Add merge function

--- dequeue/dequeue.py
+++ dequeue/dequeue.py
@@ -60,7 +60,9 @@ class Dequeue():
 
             self.merge_sort_main(l)
             self.merge_sort_main(r)
+            self.merge_sort_merge(A, l ,r)
 
+    def merge_sort_merge(self, A, l, r):
             i, j, k = 0, 0, 0
             while i < len(l) and j < len(r):
                 if l[i] < r[j]:
